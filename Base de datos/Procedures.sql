89f1ec6271d4e232390ae730fb0ed9336b71e570
jost3 <1427593@senati.pe>
2023-09-10 09:49:02 -0500

    se creo u nuevo procedemiento almacenado INSERTAR

--- Base de datos/Procedures.sql
+++ Base de datos/Procedures.sql
@@ -1,3 +1,16 @@
+--ver datos--
 CREATE PROC sp_mostrar
 AS
 SELECT * FROM estudiantes
+GO
+--Insertar datos--
+CREATE PROC insertar
+@ID INT,
+@nombre NVARCHAR(35),
+@apellido NVARCHAR(50),
+@sexo NVARCHAR(15),
+@dni CHAR(8)
+AS
+
+INSERT INTO estudiantes VALUES (@ID, @nombre,@apellido,@sexo,@dni)
+GO
