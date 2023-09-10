6deffce00241ab9f0a29d6d4abc44272b5a9c932
jost3 <1427593@senati.pe>
2023-09-10 09:02:32 -0500

    creamos una nueva clase en la capa dominio para llamr a la entidad estudiante pasra instanciarlo

---
+++ crud_capas/dominio_3/Crud/Cestudiante.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio_3.Crud
+{
+    internal class Cestudiante
+    {
+    }
+}
